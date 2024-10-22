8c5a00e34424480482552c2cf2fc3e244ee34ba0
Tony Nguyen <tnguyen411@gmail.com>
2024-10-22 15:33:59 -0700

    fix passing data from JSON to ejs view

--- json/index.js
+++ json/index.js
@@ -9,6 +9,15 @@ const port = 3000;
 const recipeJSON =
   '[{"id": "0001","type": "taco","name": "Chicken Taco","price": 2.99,"ingredients": {"protein": {"name": "Chicken","preparation": "Grilled"},  "salsa": {"name": "Tomato Salsa","spiciness": "Medium"},  "toppings": [{"name": "Lettuce",  "quantity": "1 cup",  "ingredients": ["Iceberg Lettuce"]  },      {"name": "Cheese",  "quantity": "1/2 cup",  "ingredients": ["Cheddar Cheese", "Monterey Jack Cheese"]  },      {"name": "Guacamole",  "quantity": "2 tablespoons",  "ingredients": ["Avocado", "Lime Juice", "Salt", "Onion", "Cilantro"]  },      {"name": "Sour Cream",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream"]  }      ]    }  },{"id": "0002","type": "taco","name": "Beef Taco","price": 3.49,"ingredients": {"protein": {"name": "Beef","preparation": "Seasoned and Grilled"},  "salsa": {"name": "Salsa Verde","spiciness": "Hot"},  "toppings": [{"name": "Onions",  "quantity": "1/4 cup",  "ingredients": ["White Onion", "Red Onion"]  },      {"name": "Cilantro",  "quantity": "2 tablespoons",  "ingredients": ["Fresh Cilantro"]  },      {"name": "Queso Fresco",  "quantity": "1/4 cup",  "ingredients": ["Queso Fresco"]  }      ]    }  },{"id": "0003","type": "taco","name": "Fish Taco","price": 4.99,"ingredients": {"protein": {"name": "Fish","preparation": "Battered and Fried"},  "salsa": {"name": "Chipotle Mayo","spiciness": "Mild"},  "toppings": [{"name": "Cabbage Slaw",  "quantity": "1 cup",  "ingredients": [    "Shredded Cabbage",    "Carrot",    "Mayonnaise",    "Lime Juice",    "Salt"          ]  },      {"name": "Pico de Gallo",  "quantity": "1/2 cup",  "ingredients": ["Tomato", "Onion", "Cilantro", "Lime Juice", "Salt"]  },      {"name": "Lime Crema",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream", "Lime Juice", "Salt"]  }      ]    }  }]';
 
+
+// function getTopping(obj) {
+//   let toppings = [];
+//   for (let topping in obj) {
+//     toppings.push(topping.name); 
+//   }
+//   return toppings;
+// }
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -19,23 +28,27 @@ app.get("/", (req, res) => {
 app.post("/recipe", (req, res) => {
   //Step 3: Write your code here to make this behave like the solution website.
   const recipeObj = JSON.parse(recipeJSON);
+  const value = req.body.value;
 
-  if (req.body.value == "chicken") {
+  if (req.body.choice == "chicken") {
     res.render("index.ejs", 
       {
-        recipe: recipeObj[0]
+        recipe: recipeObj[0],
+        toppings: recipeObj[2].ingredients.toppings
       }
     )
-  } else if (req.body.value == "beef") {
+  } else if (req.body.choice == "beef") {
     res.render("index.ejs", 
       {
-        recipe: recipeObj[1]
+        recipe: recipeObj[1],
+        toppings: recipeObj[2].ingredients.toppings
       }
     )
   } else {
     res.render("index.ejs",
       {
-        recipe: recipeObj[2]
+        recipe: recipeObj[2],
+        toppings: recipeObj[2].ingredients.toppings
       }
     )
   }
